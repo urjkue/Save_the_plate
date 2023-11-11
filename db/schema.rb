80f372072e1b184f04364764a8a96bbe3d1f4782
warrennookooloo <kw.warren98@gmail.com>
2023-11-11 15:36:00 +0400

    validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_11_074707) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_11_111303) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_11_074707) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "business_id", null: false
+    t.datetime "pickup"
     t.index ["business_id"], name: "index_baskets_on_business_id"
   end
 
@@ -53,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_11_074707) do
     t.bigint "business_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "pickup"
     t.index ["basket_id"], name: "index_carts_on_basket_id"
     t.index ["business_id"], name: "index_carts_on_business_id"
     t.index ["user_id"], name: "index_carts_on_user_id"
@@ -73,7 +75,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_11_074707) do
     t.bigint "business_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "users_id", null: false
     t.index ["business_id"], name: "index_reviews_on_business_id"
+    t.index ["users_id"], name: "index_reviews_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -102,4 +106,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_11_074707) do
   add_foreign_key "favourits", "baskets"
   add_foreign_key "favourits", "users"
   add_foreign_key "reviews", "businesses"
+  add_foreign_key "reviews", "users", column: "users_id"
 end
