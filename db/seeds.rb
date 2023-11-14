efaea2bd7ac97f9cc3a8218209c62cef091c1d06
DakshTorul <toruldaksh@gmail.com>
2023-11-14 18:40:23 +0400

    check

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,70 +81,70 @@
 # # db/seeds.rb
 
 # Seed Users
-5.times do
-  User.create(
-    email: Faker::Internet.email,
-    password: 'password',
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    address: Faker::Address.full_address,
-    latitude: Faker::Address.latitude,
-    longitude: Faker::Address.longitude
-  )
-end
+# 5.times do
+#   User.create(
+#     email: Faker::Internet.email,
+#     password: 'password',
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     address: Faker::Address.full_address,
+#     latitude: Faker::Address.latitude,
+#     longitude: Faker::Address.longitude
+#   )
+# end
 
-# Seed Businesses
-categories = ['restaurant', 'bakery', 'supermarket']
+# # Seed Businesses
+# categories = ['restaurant', 'bakery', 'supermarket']
 
-categories.each do |category|
-  5.times do
-    business = Business.create(
-      category: category,
-      name: Faker::Company.name,
-      description: Faker::Company.catch_phrase,
-      address: Faker::Address.full_address,
-      latitude: Faker::Address.latitude,
-      longitude: Faker::Address.longitude
-    )
+# categories.each do |category|
+#   5.times do
+#     business = Business.create(
+#       category: category,
+#       name: Faker::Company.name,
+#       description: Faker::Company.catch_phrase,
+#       address: Faker::Address.full_address,
+#       latitude: Faker::Address.latitude,
+#       longitude: Faker::Address.longitude
+#     )
 
-    # Seed Baskets
-    basket = Basket.create(
-      name: "Basket for #{business.name}",
-      description: Faker::Lorem.sentence,
-      price: Faker::Commerce.price,
-      availability: Faker::Date.forward(days: 30),
-      business: business
-    )
+#     # Seed Baskets
+#     basket = Basket.create(
+#       name: "Basket for #{business.name}",
+#       description: Faker::Lorem.sentence,
+#       price: Faker::Commerce.price,
+#       availability: Faker::Date.forward(days: 30),
+#       business: business
+#     )
 
-    # Associate the Basket with the Business
-    business.update(basket: basket)
+#     # Associate the Basket with the Business
+#     business.update(basket: basket)
 
-    # Seed Reviews
-    Review.create(
-      comment: Faker::Lorem.paragraph,
-      rating: rand(1..5).to_f,
-      business: business,
-      user: User.all.sample
-    )
+#     # Seed Reviews
+#     Review.create(
+#       comment: Faker::Lorem.paragraph,
+#       rating: rand(1..5).to_f,
+#       business: business,
+#       user: User.all.sample
+#     )
 
-    # Seed Favourites
-    Favourit.create(
-      basket: basket,
-      user: User.all.sample
-    )
+#     # Seed Favourites
+#     Favourit.create(
+#       basket: basket,
+#       user: User.all.sample
+#     )
 
-    # Seed Carts
-    Cart.create(
-      user: User.all.sample,
-      basket: basket,
-      pick_up: Faker::Date.forward(days: 7),
-      business: business
-    )
+#     # Seed Carts
+#     Cart.create(
+#       user: User.all.sample,
+#       basket: basket,
+#       pick_up: Faker::Date.forward(days: 7),
+#       business: business
+#     )
 
-    # Seed Bookings
-    Booking.create(
-      cart_id: Cart.all.sample.id,
-      user: User.all.sample
-    )
-  end
-end
+#     # Seed Bookings
+#     Booking.create(
+#       cart_id: Cart.all.sample.id,
+#       user: User.all.sample
+#     )
+#   end
+# end
