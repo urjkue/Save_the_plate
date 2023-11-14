626759f646f36e2dfe41e7077778aa90a697e505
DakshTorul <toruldaksh@gmail.com>
2023-11-14 21:16:41 +0400

    seed

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,4 @@
 class Business < ApplicationRecord
-  has_many :basket_id, dependent: :destroy
-  has_many :review, dependent: :destroy
+  has_many :basket_id #, dependent: :destroy
+  has_many :review#, dependent: :destroy
 end
