4d3e07d4fdee512b9b4eae5781b04ee788e07b81
DakshTorul <toruldaksh@gmail.com>
2023-11-15 21:14:11 +0400

    SAVE

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -5,15 +5,24 @@ class FavouritesController < ApplicationController
 
   end
   def new
-    
+    @fav = Favourit.new
   end
   def create
-  end
+    @fav = Favourit.new
+    @fav.user_id = current_user.id
+    @fav.basket_id = params[:basket_id]
+    if @fav.save
+      redirect_to favourites_path
+    else
+      render :new
+    end
+  end 
   def edit
   end
   def update
   end
   def destroy
+    @fav
   end
   def show
   end
