20e9be60204cd72e44ecfd3b9e86eec393f5b1f6
DakshTorul <toruldaksh@gmail.com>
2023-11-18 14:35:12 +0400

    save

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,7 @@
 class ProfileController < ApplicationController
   def index
+    @user_id = current_user.id
+    @user = User.find(@user_id)
+    @booking = Cart.where(user_id: @user_id)
   end
 end
