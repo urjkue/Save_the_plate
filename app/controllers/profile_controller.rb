c511fcb71edf41b37fa9baae8939f27eb6c3f20f
DakshTorul <toruldaksh@gmail.com>
2023-12-04 11:05:25 +0400

    save

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -10,6 +10,6 @@ class ProfileController < ApplicationController
   def bookings
     @user_id = current_user.id
     @user = User.find(@user_id)
-    @booking = Cart.where(user_id: @user_id)
+    @booking = Cart.where(user_id: @user_id).all
   end
 end
